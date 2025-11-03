41a1d0e1a664e9994c42d2a5831446997d8d8898
Seifeda <seifebekele07@gmail.com>
2025-11-03 13:52:20 +0300

    Initial Django setup with Swagger and PostgreSQL

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="ALX Travel App API",
+        title="ALX Travel API",
         default_version='v1',
-        description="API documentation for ALX Travel App",
+        description="API documentation for ALX Travel project",
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
@@ -19,7 +18,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('listings/', include('listings.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
-    # Redirect root URL to Swagger
-    path('', RedirectView.as_view(url='/swagger/', permanent=False)),
+    path('', schema_view.with_ui('swagger', cache_timeout=0)),  # 👈 Default homepage
 ]
